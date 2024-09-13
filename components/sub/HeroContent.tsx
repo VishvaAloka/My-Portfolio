f0abb31e1eb79732523be8620e8deb847a30ccaa
Vishva Aloka <rajeevaloka@gmail.com>
2024-09-13 11:50:54 +0530

    hero section

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -19,14 +19,14 @@ const HeroContent = () => {
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
-      <motion.div
-      variants={slideInFromTop}
-      className="Welcome-box py-2 px-3 border border-transparent bg-gradient-to-r from-purple-600 to-blue-600 rounded-lg shadow-md opacity-90 hover:opacity-100 transition-opacity duration-300"
-    >
-      <SparklesIcon className="text-yellow-300 mr-3 h-5 w-5 animate-pulse" />
-        <h1 className="Welcome-text text-white text-sm md:text-base font-medium tracking-wide">
-          UI/UX Designer
-        </h1>
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-2 px-3 border border-transparent bg-gradient-to-r from-purple-600 to-blue-600 rounded-lg shadow-md opacity-90 hover:opacity-100 transition-opacity duration-300"
+        >
+          <SparklesIcon className="text-yellow-300 mr-3 h-5 w-5 animate-pulse" />
+          <h1 className="Welcome-text text-white text-sm md:text-base font-medium tracking-wide">
+            UI/UX Designer
+          </h1>
         </motion.div>
 
         <motion.div
@@ -46,25 +46,25 @@ const HeroContent = () => {
               className="mb-6 text-[35px] lg:text-[58px] font-secondary font-semibold uppercase leading-[1]"
             >
               <span className="text-white mr-3">I am a</span>
-        <TypeAnimation
-        sequence={[
-          "UI/UX Designer",
-          2000,
-          "Frontend Developer",
-          2000,
-          "Website Designer",
-          2000,
-          "Creative Coder",
-          2000,
-          "Innovator",
-          2000,
-        ]}
-        speed={50}
-        deletionSpeed={30}
-        className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-cyan-500 text-6xl font-bold animate-fadeIn"
-        wrapper="span"
-        repeat={Infinity}
-        /> 
+              <TypeAnimation
+                sequence={[
+                  "UI/UX Designer",
+                  2000,
+                  "Frontend Developer",
+                  2000,
+                  "Website Designer",
+                  2000,
+                  "Creative Coder",
+                  2000,
+                  "Innovator",
+                  2000,
+                ]}
+                speed={50}
+                deletionSpeed={30}
+                className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-cyan-500 text-6xl font-bold animate-fadeIn"
+                wrapper="span"
+                repeat={Infinity}
+              />
             </motion.div>
             About
           </span>
@@ -74,13 +74,13 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          Hello! I'm Vishva Aloka, an undergraduate student at the
-          SriLanka Information Institute of Technology, specializing in
-          Information Technology. I approach my assignments with creativity and
-          attention to detail since I am passionate about remaining up to date
-          in the field of web development design. Excited to collaborate with
-          fellow professionals and contribute to innovative initiatives in the
-          IT industry.
+          Hello! I'm Vishva Aloka, an undergraduate student at the SriLanka
+          Information Institute of Technology, specializing in Information
+          Technology. I approach my assignments with creativity and attention to
+          detail since I am passionate about remaining up to date in the field
+          of web development design. Excited to collaborate with fellow
+          professionals and contribute to innovative initiatives in the IT
+          industry.
         </motion.p>
         <motion.a
           href="/CV.pdf"
